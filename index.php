1f3e5d86a6e54dce5a0636b5dfd6595ee250b3bc
Adan Altamira <holler@adanaltamira.com>
2012-06-06 21:33:57 -0500

    Establishing an HTML document.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php echo="fuck you kyle" ?>
-
-<p>Boom!</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="author" content="Adan Altamira & Kyle Weber" />
+	<meta name="description" content="A geolocation mobile web app experiment." />
+	<title>Locator</title>
+</head>
+<body>
+</body>
+</html>
