12ac22a97e1d1c9978fcfa8236262e7aa89ae033
Adan Altamira <holler@adanaltamira.com>
2012-06-09 18:58:24 -0500

    Adding semantic HTML 5 containers for content with RIA attributes for accessbility

--- index.php
+++ index.php
@@ -2,10 +2,33 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<meta name="author" content="Adan Altamira & Kyle Weber" />
+	<meta name="author" content="Adan Altamira &amp; Kyle Weber" />
 	<meta name="description" content="A geolocation mobile web app experiment." />
 	<title>Locator</title>
 </head>
 <body>
+	<header role="banner">
+		<hgroup>
+			<h1>Locator</h1>
+			<h2>Geolocation Experiment</h2>
+		</hgroup>
+		<section id="enable-message" class="hidden">JavaScript is disabled please enable to unleash awesome.</section>
+	</header>
+	<section role="main">
+		<article>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat congue mauris vel pellentesque. Phasellus quis lorem lorem. Curabitur nec eleifend leo. Vestibulum et ipsum vitae mauris tempus imperdiet eu eu metus. Maecenas sit amet mollis nisl. Aliquam aliquam aliquam ligula, non venenatis elit pretium eget. Vivamus id orci mauris. Suspendisse ante ante, pretium et placerat ut, placerat in libero. Nulla facilisi. Nulla eu tellus urna.</p>
+		</article>
+		<article id="location-prompt">
+			<p>We're stalking you.</p>
+			<section>
+				<p>Please enable location in your browser. Press the button.</p>
+				<input type="button" value="Locate!">
+			</section>
+		</article>
+		<section id="map"></section>
+	</section>
+	<footer role="conteninfo">
+		<p>&copy; 2012 and Forever. Kyle Weber &amp; Adan Altamira.</p>
+	</footer>
 </body>
 </html>
